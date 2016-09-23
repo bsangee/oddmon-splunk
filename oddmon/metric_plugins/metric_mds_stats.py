6b773f3594a838eec49a02183b42b76070a3bd84
Sangeetha <bsangee@vt.edu>
2016-09-23 13:46:55 -0400

    Update metric_mds_stats.py

--- oddmon/metric_plugins/metric_mds_stats.py
+++ oddmon/metric_plugins/metric_mds_stats.py
@@ -2,6 +2,7 @@
 
 import os
 import logging
+import ConfigParser
 import json
 import time
 from collections import defaultdict
@@ -21,19 +22,42 @@ class G:
 
 def metric_init(name, config_file, is_subscriber=True,
                 loglevel=logging.DEBUG):
-    global logger
+    global logger, stats_logger
     logger = logging.getLogger("app.%s" % __name__)
     rv = True
-    
-    G.fsname, G.mdtnames = lfs_utils.scan_targets(OSS=False)
-    if not G.mdtnames:
-        logger.warn("No MDT's found.  Disabling plugin.")
-        rv = False
-    elif not G.fsname:
-        logger.error("MDT's found, but could not discern filesystem name. "
-                     "(This shouldn't happen.)  Disabling plugin.")
-        rv = False
-    
+    if is_subscriber is True:
+        G.fsname, G.mdtnames = lfs_utils.scan_targets(OSS=False)
+        if not G.mdtnames:
+                logger.warn("No MDT's found.  Disabling plugin.")
+                rv = False
+        elif not G.fsname:
+                logger.error("MDT's found, but could not discern filesystem name. "
+                             "(This shouldn't happen.)  Disabling plugin.")
+                rv = False
+        # config file is only needed for the location of the
+        # stats_logger file, and that's only needed on the
+        # subscriber side
+        config = ConfigParser.SafeConfigParser()
+        try:
+            config.read(config_file)
+            G.save_dir = config.get(name, "save_dir")
+        except Exception, e:
+            logger.error("Can't read configuration file")
+            logger.error("Exception: %s" % e)
+            rv = False
+        # TODO: this code block should probably be inside the exception handler
+        # log to file until reaching maxBytes then create a new log file
+        stats_logger = logging.getLogger("mds_stats.%s" % __name__)
+        stats_logger.propagate = False
+        stats_logger_name = G.save_dir+os.sep+"mds_stats_log.txt"
+        logger.debug("Stats data saved to: %s" % stats_logger_name)
+        stats_logger.addHandler(
+            logging.handlers.RotatingFileHandler(stats_logger_name,
+                                                 maxBytes=1024*1024*1024,
+                                                 backupCount=1))
+        stats_logger.setLevel(logging.DEBUG)
+
+
     return rv
 
 
@@ -55,16 +79,10 @@ def get_stats():
 def save_stats(msg):
     stats = json.loads(msg)
 
-    for target in stats.keys():
-        jobList = stats[target]
-        for job in jobList:
-            # convert the python structure into an event string suitable
-            # for Splunk and write it out
-                event_str = "cpu=%f  mem=%d MDS=%s" %\
-                            (float(job["cpu"]), int(job["mem"]), str(target))
+    event_str = "snapshot: %d" % int(time.time())
+    stats_logger.info(event_str)
 
-		stats_logger.info(event_str)
-
+    stats_logger.info(stats)
 
 
 def read_mds_stats(f):
@@ -85,7 +103,7 @@ def read_mds_stats(f):
 
 def update():
 
-	ret = read_mds_stats()
+        ret = read_mds_stats()
         if ret:
             G.stats = ret
 
@@ -95,3 +113,4 @@ if __name__ == '__main__':
         print get_stats()
         time.sleep(5)
     metric_cleanup()
+                                              
