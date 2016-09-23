157e391a2edc2414671a3665849a42ae2c434cdf
Sangeetha <bsangee@vt.edu>
2016-09-23 13:53:56 -0400

    Update metric_mds_lnet_stats.py

--- oddmon/metric_plugins/metric_mds_lnet_stats.py
+++ oddmon/metric_plugins/metric_mds_lnet_stats.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import ConfigParser
 import logging
 import json
 import time
@@ -21,19 +22,46 @@ class G:
 
 def metric_init(name, config_file, is_subscriber=True,
                 loglevel=logging.DEBUG):
-    global logger
+    global logger, stats_logger
     logger = logging.getLogger("app.%s" % __name__)
     rv = True
-    
-    G.fsname, G.mdtnames = lfs_utils.scan_targets(OSS=False)
-    if not G.mdtnames:
-        logger.warn("No MDT's found.  Disabling plugin.")
-        rv = False
-    elif not G.fsname:
-        logger.error("MDT's found, but could not discern filesystem name. "
-                     "(This shouldn't happen.)  Disabling plugin.")
-        rv = False
-    
+
+    if is_subscriber is True:
+        G.fsname, G.mdtnames = lfs_utils.scan_targets(OSS=False)
+        if not G.mdtnames:
+                logger.warn("No MDT's found.  Disabling plugin.")
+                rv = False
+        elif not G.fsname:
+                logger.error("MDT's found, but could not discern filesystem name. "
+                             "(This shouldn't happen.)  Disabling plugin.")
+                rv = False
+        # config file is only needed for the location of the
+        # stats_logger file, and that's only needed on the
+        # subscriber side
+        config = ConfigParser.SafeConfigParser()
+        try:
+            config.read(config_file)
+            G.save_dir = config.get(name, "save_dir")
+        except Exception, e:
+            logger.error("Can't read configuration file")
+            logger.error("Exception: %s" % e)
+            rv = False
+
+        # TODO: this code block should probably be inside the exception handler
+        # log to file until reaching maxBytes then create a new log file
+        stats_logger = logging.getLogger("mds_lnet_stats.%s" % __name__)
+        stats_logger.propagate = False
+        stats_logger_name = G.save_dir+os.sep+"mds_lnet_stats_log.txt"
+        logger.debug("Stats data saved to: %s" % stats_logger_name)
+        stats_logger.addHandler(
+            logging.handlers.RotatingFileHandler(stats_logger_name,
+                                                 maxBytes=1024*1024*1024,
+                                                 backupCount=1))
+        stats_logger.setLevel(logging.DEBUG)
+
+
+
+
     return rv
 
 
@@ -54,21 +82,9 @@ def get_stats():
 
 def save_stats(msg):
     stats = json.loads(msg)
-    # msg (and thus stats) is organized as a dictionary with target (OST or
-    # MDT) names for keys.  The associated values are themselves lists of
-    # dictionaries where each dictionary is the key/value data for one job.
-
-    for target in stats.keys():
-        jobList = stats[target]
-        for job in jobList:
-            # convert the python structure into an event string suitable
-            # for Splunk and write it out
-                event_str = "  send_count=%d  recv_count=%d send_length=%d recv_length=%d MDS=%s" %\
-                            (int(job["send_count"]), int(job["recv_count"]),
-                             int(job["send_length"]), int(job["recv_length"]), str(target))
-
-    		stats_logger.info(event_str)
-
+    event_str = "snapshot: %d" % int(time.time())
+    stats_logger.info(event_str)
+    stats_logger.info(stats)
 
 def read_lnet_stats(f):
     """
